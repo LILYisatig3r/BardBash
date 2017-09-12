02bd642e01ab23a5e79df87c6f6fd7aeb75d503a
Christian <christian.50.roy@gmail.com>
2017-09-11 23:14:17 -0600

    Add Basic Player Movement and Walls

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed = 5.0f;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update ()
+    {
+        _rigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, Input.GetAxisRaw("Vertical") * moveSpeed);
+
+        if (_rigidbody.velocity.x < 0)
+        {
+            // Face left
+        }
+        else
+        {
+            // Face right
+        }
+    }
+
+}
