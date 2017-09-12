28d0abbb525dbf3b4cf89af796bdffaf880159f0
Christian <christian.50.roy@gmail.com>
2017-09-12 00:02:23 -0600

    Add Enemy Framework

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public int aggroRange = 3;
+
+    private CircleCollider2D _aggroCollider;
+
+    private void Start()
+    {
+        _aggroCollider = GetComponent<CircleCollider2D>();
+        _aggroCollider.radius = aggroRange;
+    }
+
+    private void Update()
+    {
+        // if no target
+            // Check if player is in aggro range
+
+        // else follow target
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_aggroCollider == null)
+        {
+            _aggroCollider = GetComponent<CircleCollider2D>();
+        }
+        
+        UnityEditor.Handles.color = Color.yellow;
+        UnityEditor.Handles.DrawWireDisc(new Vector2(_aggroCollider.transform.position.x, _aggroCollider.transform.position.y) + _aggroCollider.offset, Vector3.back, aggroRange);
+        
+    }
+}
